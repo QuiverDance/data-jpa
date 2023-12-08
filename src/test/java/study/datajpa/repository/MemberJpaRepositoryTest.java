c3c9fac714bb91994da45815bcd13e163b5f78c5
여름 <pjw7200@ajou.ac.kr>
2023-12-08 16:16:29 +0900

    add spring data jpa basic test

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -23,5 +25,30 @@ class MemberJpaRepositoryTest {
         Member foundMember = memberJpaRepository.find(savedMember.getId());
 
         Assertions.assertThat(foundMember.getId()).isEqualTo(savedMember.getId());
+    }
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member foundMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member foundMember2 = memberJpaRepository.findById(member2.getId()).get();
+        Assertions.assertThat(foundMember1).isEqualTo(member1);
+        Assertions.assertThat(foundMember2).isEqualTo(member2);
+
+        List<Member> all = memberJpaRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deleteCount = memberJpaRepository.count();
+        Assertions.assertThat(deleteCount).isEqualTo(0);
     }
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 @SpringBootTest
 @Transactional
 class MemberRepositoryTest {
@@ -21,5 +23,30 @@ class MemberRepositoryTest {
         Member foundMember = memberRepository.findById(savedMember.getId()).get();
 
         Assertions.assertThat(foundMember.getId()).isEqualTo(savedMember.getId());
+    }
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member foundMember1 = memberRepository.findById(member1.getId()).get();
+        Member foundMember2 = memberRepository.findById(member2.getId()).get();
+        Assertions.assertThat(foundMember1).isEqualTo(member1);
+        Assertions.assertThat(foundMember2).isEqualTo(member2);
+
+        List<Member> all = memberRepository.findAll();
+        Assertions.assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deleteCount = memberRepository.count();
+        Assertions.assertThat(deleteCount).isEqualTo(0);
     }
 }
